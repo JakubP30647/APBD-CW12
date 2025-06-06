02fa048e24108c27d9119e6855a3ebaa97c04598
JakubP30647 <s30647@pjwstk.edu.pl>
2025-06-06 04:04:59 +0200

    Troche lepiej

--- APBD-CW12/Services/ClientsService.cs
+++ APBD-CW12/Services/ClientsService.cs
@@ -23,15 +23,18 @@ public class ClientsService:IClientsService
         await using IDbContextTransaction transaction = await _context.Database.BeginTransactionAsync();
         try
         {
-            var c = _context.Clients
-                .Include(c => c.ClientTrips)
-                .FirstOrDefault(c => c.IdClient == id);
+            var c = await _context.Clients
+                .FindAsync(id);
+            
             if (c == null)
             {
                 throw new NotFoundException($"Client with id: {id} not found");
             }
 
-            if (c.ClientTrips.Count >= 1)
+            
+            bool a = await _context.ClientTrips.AnyAsync(c => c.IdClient == id);
+            
+            if (a)
             {
                 throw new ClientHasTripsException($"Client with id: {id}, has trips and can't be deleted");
             }
